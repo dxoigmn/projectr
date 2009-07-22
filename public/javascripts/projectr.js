988ae1d173e0c5ac6c794fd20c9cb4b560c094f0
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2009-07-22 06:08:05 -0400

    Cleanup ajax loading code and add loading indicator.

--- public/javascripts/projectr.js
+++ public/javascripts/projectr.js
@@ -4,38 +4,24 @@ $(document).ready(function() {
   $(".date").relatizeDate();
   
   function retrieveCommit() {
-    $.getJSON($('.commit:last').attr('id') + '.js', function(commits) {
-      $.each(commits, function(index, commit) {
+    $('.commit:last').after('<img src="/images/loading.gif" class="loading"/>')
+    
+    $.get($('.commit:last').attr('id'), function(page) {
+      $(page).find('div.commit').each(function(index, commit) {
         if (index == 0) {
           return;
         }
         
-        $('.commit:last').after("\
-        <div class='commit' id='" + commit.id + "'>\
-          <h3>" + commit.sha + "</h3>\
-          <div class='author'>\
-            <h4>Author</h4>\
-            <img src='" + commit.author_gravatar + "' />\
-            <div class='name'>" + commit.author_name + "</div>\
-            <div class='date'>" + commit.authored_date + "</div>\
-          </div>\
-          <div class='committer" + (commit['by_author?'] ? ' is_author' : '') + "'>\
-            <h4>Committer</h4>\
-            <img src='" + commit.committer_gravatar + "' />\
-            <div class='name'>" + commit.committer_name + "</div>\
-            <div class='date'>" + commit.committed_date + "</div>\
-          </div>\
-          <div class='message'>" + commit.message + "</div>\
-          <div class='clear'></div>\
-        </div>\
-        ");
-        $(".date").relatizeDate();
-      })
+        $('.commit:last').after(commit);
+      });
+      
+      $(".date").relatizeDate();
+      $('img.loading').remove();
     });
   }
   
   $(window).scroll(function(){
-    if  ($(window).scrollTop() == $(document).height() - $(window).height()){
+    if ($(window).scrollTop() == $(document).height() - $(window).height()){
        retrieveCommit();
     }
   });
